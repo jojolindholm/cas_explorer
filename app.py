15d8dc7bff1e88ad605a446259b8412c372d174c
Johan Lindholm <48241545+jojolindholm@users.noreply.github.com>
2024-04-29 08:54:25 +0200

    Fixed delist only problem

--- app.py
+++ app.py
@@ -7,6 +7,7 @@ df = pd.read_csv("info_arbitrators.csv")
 
 # Add dropdown menu for selection category
 delisted_checkbox = st.checkbox("Delisted")
+listed_checkbox = not st.checkbox("Listed")
 
 # Add sliders for customizing the data
 years_listed_min = np.nanmin(df["years_listed"])
@@ -37,7 +38,7 @@ delisted_age_range = st.slider('Delisted age:',
 filtered_df = df[(df['years_listed'] >= years_served_range[0]) & (df['years_listed'] <= years_served_range[1]) & 
                 (df['n_cases'] >= n_cases_range[0]) & (df['n_cases'] <= n_cases_range[1]) &
                 (df["delisted_age"] >= delisted_age_range[0]) & (df["delisted_age"] <= delisted_age_range[1]) &
-                (df["delisted"] == delisted_checkbox)]
+                (df["delisted"] == delisted_checkbox) | (df["delisted"] == listed_checkbox)]
 
 # Display filtered data
 st.write(filtered_df)
